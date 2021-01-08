baa6fda1004ae9a90cb708aaac868578d8ec430f
mishell <mmq.llumitaxi@yavirac.edu.ec>
2021-01-07 22:45:10 -0500

    Agregar archivos de proyecto.

---
+++ SRP_EJEMPLO/Factura.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejemplo
+{
+    class Factura
+    {
+        public string Codigo { get; set; }
+        public DateTime FechaEmision { get; set; }
+        public decimal ImporteFactura { get; set; }
+        public decimal ImporteIVA { get; set; }
+        public decimal ImporteDeduccion { get; set; }
+        public decimal ImporteTotal { get; set; }
+        public decimal PorcentajeDeduccion { get; set; }
+
+
+        // Método que calcula el total de la factura
+        public void CalcularTotal()
+        {
+         
+            ImporteDeduccion = (ImporteFactura * PorcentajeDeduccion) / 100;
+        
+            ImporteIVA = ImporteFactura * 0.12m;
+  
+            ImporteTotal = (ImporteFactura - ImporteDeduccion) + ImporteIVA;
+        }
+    }
+}
---
+++ SRP_EJEMPLO/FacturaFactorizada.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ejemplo
+{
+    class FacturaFactorizada
+    {
+        public string Codigo { get; set; }
+        public DateTime FechaEmision { get; set; }
+        public decimal ImporteFactura { get; set; }
+        public decimal ImporteIVA { get; set; }
+        public decimal ImporteDeduccion { get; set; }
+        public decimal ImporteTotal { get; set; }
+        public decimal PorcentajeDeduccion { get; set; }
+
+
+        public void CalcularTotal()
+        {
+
+            Deduccion deduccion = new Deduccion(PorcentajeDeduccion);
+            ImporteDeduccion = deduccion.CalcularDeduccion(ImporteFactura);
+
+            Iva iva = new Iva();
+            ImporteIVA = iva.CalcularIVA(ImporteFactura);
+
+            ImporteTotal = (ImporteFactura - ImporteDeduccion) + ImporteIVA;
+        }
+    }
+}
